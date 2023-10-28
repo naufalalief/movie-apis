f93d26bb8a3e5cd6a972068b6276ff0bb05273fa
Naufal Alief <naufalalief087@gmail.com>
2023-10-29 04:09:33 +0700

    added config

--- config/db.go
+++ config/db.go
@@ -9,21 +9,21 @@ import (
 	"gorm.io/gorm"
 )
 
-func ConnectDataBase() *gorm.DB {
+func ConnectDatabase() *gorm.DB {
 	username := utils.GetEnv("DATABASE_USERNAME", "root")
-	password := utils.GetEnv("DATABASE_PASSWORD", "12356")
+	password := utils.GetEnv("DATABASE_PASSWORD", "123456")
 	host := utils.GetEnv("DATABASE_HOST", "127.0.0.1")
 	port := utils.GetEnv("DATABASE_PORT", "3306")
 	database := utils.GetEnv("DATABASE_NAME", "database_movies")
 
-	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, port, database)
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
+		username, password, host, port, database)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic(err.Error())
 	}
-
-	db.AutoMigrate(&models.User{}, &models.Movie{}, &models.AgeRatingCategory{})
+	db.AutoMigrate(&models.Movie{}, &models.AgeRatingCategory{}, &models.User{})
 
 	return db
 }
